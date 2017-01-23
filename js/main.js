7e15cfab20f7dcd69735b5133c9a4eb758a4fc55
Justin Russo <firstnecron@gmail.com>
2017-01-23 15:40:32 -0500

    Display text input when "other" is selected for job role

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
+// Job Role
+var $title = $('#title');
+var $otherJobRole = $('<label for="title_other">Your Job Role:</label>' +
+    '<input type="text" id="title_other" name="user_title_other">');
+
+var jobRoleSelected = function () {
+    // Add otherJobRole text input when "other" is selected as the job role's value
+    // Otherwise, remove it
+    if (this.value === 'other') {
+        $title.after($otherJobRole);
+    } else {
+        $otherJobRole.remove();
+    }
+};
+
+$title.on('change', jobRoleSelected);
+
 window.onload = function() {
     // Focus Name field
     $('#name').focus();
