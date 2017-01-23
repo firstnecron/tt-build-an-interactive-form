856af461b47bfa96b31ec760aebf492bb564a284
Justin Russo <firstnecron@gmail.com>
2017-01-23 16:12:14 -0500

    Hide color drop down menu until a T-shirt design is selected Hide color options until a T-shirt design is selected

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ var $title = $('#title');
 var $otherJobRole = $('<label for="title_other">Your Job Role:</label>' +
     '<input type="text" id="title_other" name="user_title_other">');
 
+// T-Shirt
+var $design = $('#design');
+var $color = $('#color').hide(); // Start color hidden
+var $colorPlaceholder = $('<option>Please select a T-shirt theme</option>');
+var colorOptions = $color.find('option');
+
 var jobRoleSelected = function () {
     // Add otherJobRole text input when "other" is selected as the job role's value
     // Otherwise, remove it
@@ -15,7 +21,40 @@ var jobRoleSelected = function () {
     }
 };
 
+var designSelected = function() {
+    // If the color selection is hidden (assume label is too), show them.
+    if ($color.is(':hidden')) {
+        $('label[for=color]').show();
+        $color.show();
+    }
+
+    switch (this.value) {
+        case 'js puns':
+            break;
+        case 'heart js':
+            break;
+        default:
+            // No design selected
+            // Hide colors
+            hideColorOptions();
+            break;
+    }
+};
+
+// Hide color options and display placeholder option.
+var hideColorOptions = function() {
+    $color.empty();
+    $color.append($colorPlaceholder);
+};
+
+// When job role selection is changed, run jobRoleSelected
 $title.on('change', jobRoleSelected);
+
+// When the t-shirt design option is changed, run designSelected
+$design.on('change', designSelected);
+
+// Hide color's label initially
+$('label[for=color]').hide();
 
 window.onload = function() {
     // Focus Name field
