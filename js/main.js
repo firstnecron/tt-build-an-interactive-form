9eb1cadac2bc534fd79375118ceef16eeec97de0
Justin Russo <firstnecron@gmail.com>
2017-01-27 20:50:26 -0500

    Form validation Prevent submission until form is valid Display error messages in error box (adjusting based on error) Display activities requirement in real time in the form Indicate errors on submission

--- js/main.js
+++ js/main.js
@@ -16,8 +16,21 @@ var colorOptions = {
 // Activities
 var $activities = $('.activities');
 var activityOptions = [];
-var $total = $('<div id="total"></div>').hide();
+var $total = $('<div id="total"></div>');
 var totalCost = 0;
+
+// Errors / Validation
+var $errorBox = $(
+    '<div id="error-box">' +
+        '<div class="fixed-error-box">' +
+            '<ul id="errors">' +
+                '<li id="name-error"><span class="is-valid x-mark"></span> Name is entered</li>' +
+                '<li id="email-error"><span class="is-valid x-mark"></span> Email is entered</li>' +
+                '<li id="activity-error"><span class="is-valid x-mark"></span> One or more activity is selected</li>' +
+            '</ul>' +
+        '</div>' +
+    '</div>');
+var $activityErrorMessage = $('<div id="activity-error-message" class="text-error">At least one activity must be selected</div>');
 
 var jobRoleSelected = function () {
     // Add otherJobRole text input when "other" is selected as the job role's value
@@ -114,7 +127,6 @@ var handleActivityConflicts = function (activity, disable) {
         if (activityOptions[i].name.toLowerCase() !== activity.name.toLowerCase()) {
             // If times match
             if (activityOptions[i].time === activity.time) {
-                console.log('match');
                 if (disable === true) {
                     // Disable conflicts
                     activityOptions[i].element.addClass('text-muted');
@@ -147,6 +159,183 @@ var handleActivityCost = function (activity, add) {
     }
 };
 
+// Name validation
+var isNameValid = function () {
+    var isValid = true;
+
+    var name = $('#name').val();
+    if (!name || name === '') {
+        isValid = false;
+    }
+
+    return isValid;
+};
+
+// Email validation
+var isEmailValid = function () {
+    var isValid = true;
+
+    var email = $('#mail').val();
+    // Check if blank
+    if (!email || email === '') {
+        isValid = false;
+    } else {
+        // See if email address entered is invalid (if it does not match)
+        if (!email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
+};
+
+// Activities validation (At least one must be selected)
+var isActivitiesValid = function () {
+    var isValid = true;
+
+    if ($('.activities input').filter(':checked').length <= 0) {
+        // If there are none (or somehow less) selectedisValid = false;
+    }
+
+    return isValid;
+};
+
+// Credit Card validation
+var isPaymentNumberValid = function () {
+    var isValid = true;
+
+    //16-digit credit card number
+    var ccNum = $('#cc-num').val();
+    if (!ccNum || ccNum === '') {
+        // Credit card number is empty
+        isValid = false;
+    } else {
+        // See if credit card number entered is invalid (if it does not match)
+        if (!ccNum.match(/^[\d]{16}$/)) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
+};
+
+var isPaymentZipValid = function () {
+    var isValid = true;
+
+    // 5-digit zip code
+    var zip = $('#zip').val();
+    if (!zip || zip === '') {
+        // Zip code is empty
+        isValid = false;
+    } else {
+        // See if zip code entered is invalid (if it does not match)
+        if (!zip.match(/^[\d]{5}$/)) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
+};
+
+var isPaymentCvvValid = function() {
+    var isValid = true;
+
+    // 3-digit CVV
+    var cvv = $('#cvv').val();
+    if (!cvv || cvv === '') {
+        // CVV is empty
+        isValid = false;
+    } else {
+        // See if CVV entered is invalid (if it does not match)
+        if (!cvv.match(/^[\d]{3}$/)) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
+};
+
+var isPaymentValid = function () {
+    var isValid = true;
+
+    if ($('#payment').val() === 'credit card') {
+        return isPaymentNumberValid() && isPaymentZipValid() && isPaymentCvvValid();
+    }
+
+    return isValid;
+};
+
+// Validates if all required form fields are filled (and filled properly)
+var isFormValid = function () {
+    return isNameValid() && isEmailValid() && isActivitiesValid() && isPaymentValid();
+};
+
+// Toggle between X and Check mark for an error message, given the id of the error message and if it is valid
+var toggleValidationMessage = function (target, isValid) {
+    var $target = $(target + ' span.is-valid');
+    if ($target.hasClass('x-mark') && isValid) {
+        $target.removeClass('x-mark');
+        $target.addClass('check-mark');
+    } else if ($target.hasClass('check-mark') && !isValid) {
+        $target.removeClass('check-mark');
+        $target.addClass('x-mark');
+    }
+};
+
+// Insert a validation message after target
+// Target - target to insert after
+// ID - id of the new message list item
+// Message - message to display
+var insertValidationMessage = function(target, id, message) {
+    $(target).after('<li id="' + id + '" class="fadeInDown animated"><span class="is-valid x-mark"></span> ' + message + '</li>');
+};
+
+// Toggles activity requirement displayed directly in the form depending on if it is valid
+var toggleActivityFormMessage = function (isValid) {
+    if (isValid) {
+        $activityErrorMessage.hide();
+    } else {
+        $activityErrorMessage.show();
+    }
+};
+
+var handleSubmitError = function () {
+    // Clear all input-error classes
+    var pastInvalids = $('.input-error');
+    if (pastInvalids.length > 0) {
+        for (var i = 0; i < pastInvalids.length; i++) {
+            $(pastInvalids[i]).removeClass('input-error');
+        }
+    }
+
+    // Check for invalids and add input-error classes
+
+    if (!isNameValid()) {
+        $('#name').addClass('input-error');
+    }
+
+    if (!isEmailValid()) {
+        $('#mail').addClass('input-error');
+    }
+
+    // Skip activities - this is done in real time
+
+    // Validate if credit card is selected
+    if ($('#payment').val() === 'credit card') {
+        if (!isPaymentNumberValid()) {
+            $('#cc-num').addClass('input-error');
+        }
+
+        if (!isPaymentZipValid()) {
+            $('#zip').addClass('input-error');
+        }
+
+        if (!isPaymentCvvValid()) {
+            $('#cvv').addClass('input-error');
+        }
+    }
+};
+
 // When job role selection is changed, run jobRoleSelected
 $title.on('change', jobRoleSelected);
 
@@ -159,6 +348,15 @@ $('.activities label').on('change', function () {
     handleActivityConflicts(activity, $input[0].checked); // Disable if checked
     handleActivityCost(activity, $input[0].checked); // Add if checked
 
+    // Check activity selection is valid
+    // and toggle error if needed
+    if (isActivitiesValid()) {
+        toggleActivityFormMessage(true);
+        toggleValidationMessage('#activity-error', true);
+    } else {
+        toggleActivityFormMessage(false);
+        toggleValidationMessage('#activity-error', false);
+    }
 });
 
 $('#payment').on('change', function () {
@@ -166,16 +364,75 @@ $('#payment').on('change', function () {
     $('#credit-card, #paypal, #bitcoin').filter(':visible').hide();
     // Show desired payment method
     $('#' + this.value.trim().replace(' ', '-')).show();
+
+    // Validation
+    // Show if credit card if not already, otherwise hide
+    var $paymentError = $('#payment-error');
+    if (this.value === 'credit card' && !$paymentError[0]) {
+        // Insert if error does not exist
+        insertValidationMessage('#activity-error', 'payment-error', 'Credit card information is valid');
+    } else {
+        $paymentError.remove();
+    }
 });
+
+$('#name').on('keyup', function () {
+    // Check if payment is valid and toggle error if needed
+    toggleValidationMessage('#name-error', isNameValid());
+});
+
+$('#mail').on('keyup', function () {
+    var $emailValidError = $('#email-valid-error');
+    // Check if email is empty
+    if (!this.value || this.value === '') {
+        toggleValidationMessage('#email-error', false);
+        // Remove validation error if exists
+        if ($emailValidError[0]) {
+            $emailValidError.remove();
+        }
+    } else {
+        // Insert validation error if it does not exist
+        if (!$emailValidError[0]) {
+            insertValidationMessage('#email-error', 'email-valid-error', 'Email address is valid');
+        }
+
+        // If not empty
+        toggleValidationMessage('#email-error', true);
+        // Check if email is valid and toggle error if needed
+        toggleValidationMessage('#email-valid-error', isEmailValid());
+    }
+
+});
+
+$('#cc-num, #zip, #cvv').on('keyup', function () {
+    // Check if payment is valid and toggle error if needed
+    toggleValidationMessage('#payment-error', isPaymentValid());
+});
+
+$('button[type="submit"]').on('click', function (event) {
+    // If form is not valid
+    if (!isFormValid()) {
+        event.preventDefault();
+        handleSubmitError();
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    }
+});
+
+// Remove job role other by default (support for non-javascript to see it still)
+$otherJobRole.remove();
 
 // Hide color div initially
 $('#colors-js-puns').hide();
 
 // Append total to activities, hidden by default
-$activities.append($total);
+$activities.append($total.hide());
+$activities.append($activityErrorMessage);
 
 // Hide visible payment method
 $('#credit-card, #paypal, #bitcoin').hide();
+
+// Append error box
+$('.container').append($errorBox);
 
 window.onload = function () {
     // Focus Name field
